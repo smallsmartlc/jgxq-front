d5810dccbe09e47f3f760df9ebd407a9517251ea
smallsmartlc <1346683197@qq.com>
2021-02-08 15:27:10 +0800

    图片服务器

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,7 +4,7 @@ const prodEnv = require('./dev.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  IMG_URL : '"http://localhost:6800/images/"',
+  IMG_URL : '"http://localhost:9916/"',
   BASE_URL : '"http://localhost:6800"',
   JG_COOKIE : '"smart_jgxq_cookie"',
   COOKIE_DOMAIN : '"localhost"',
--- config/prod.env.js
+++ config/prod.env.js
@@ -4,7 +4,7 @@ const prodEnv = require('./dev.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"production"',
-  IMG_URL : '"http://smallsmart.top:9920/images/"',
+  IMG_URL : '"http://smallsmart.top:9916/"',
   BASE_URL : '"http://smallsmart.top:9920"',
   JG_COOKIE : '"smart_jgxq_cookie"',
   COOKIE_DOMAIN : '"smallsmart.top"',
