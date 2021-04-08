fcee02ff9c38d879be017fc29484d3f582b6aece
smallsmartlc <1346683197@qq.com>
2021-04-08 18:34:40 +0800

    修改https配置

--- config/prod.env.js
+++ config/prod.env.js
@@ -4,7 +4,7 @@ const prodEnv = require('./dev.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"production"',
-  IMG_URL : '"https://smallsmart.top/source"',
+  IMG_URL : '"https://smallsmart.top/source/"',
   BASE_URL : '"https://smallsmart.top/api"',
   JG_COOKIE : '"smart_jgxq_cookie"',
   COOKIE_DOMAIN : '"smallsmart.top"',
