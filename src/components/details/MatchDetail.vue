62a760d126a9414f69e24a17470dfe02b7697bd0
smallsmartlc <1346683197@qq.com>
2021-08-19 17:20:05 +0800

    修改赛况的进度条

--- src/components/details/MatchDetail.vue
+++ src/components/details/MatchDetail.vue
@@ -49,16 +49,37 @@
                     <el-divider/>
                     <div></div>
                     <div class="action">
-                        <div v-if="match.actions">
-                            <div v-for="(action,index) in match.actions" :key="index">
-                                <div v-for="(item,index) in action.infoList" :key="index">
-                                    <div class="action_box">
-                                        <div>{{action.time}}′</div>
-                                        <div>{{isHome(item.home)}}</div>
-                                        <div>{{item.name}}</div>
-                                        <i :class="actionType[item.type].icon" :style="{'color':actionType[item.type].color}"></i>
+                        <div class="action-wrap" v-if="match.actions">
+                            <div class="content flex-center">
+                                <div class="line-wrapper flex-center">
+                                    <i class="el-icon-time" style="color:#fc0;font-size:20px;margin:4px"></i>
+                                    <div class="dot"></div>
+                                    <div class="line"></div>
+                                    <div v-for="(item,index) in match.actions" class="action-item" :key="index">
+                                        <div v-if="item.infoList.filter((e)=>e.home).length" class="float-wrapper">
+                                            <div class="action-list float-left">
+                                                <div v-for="(home,index) in item.infoList.filter((e)=>e.home)" class="action-detail align-right" :data-id="home.playerId" @click="toPlayerDetail" :key="index">
+                                                    <div class="icon"><i :class="actionType[home.type].icon" :style="{'color':actionType[home.type].color}" ></i></div>
+                                                    <div class="playerName">{{home.name}}</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="flex-center">
+                                            <div class="time-box">{{item.time}}</div>
+                                            <div class="line"></div>
+                                        </div>
+                                        <div v-if="item.infoList.filter((e)=>!e.home).length" class="float-wrapper">
+                                            <div class="action-list float-right">
+                                                <div v-for="(visit,index) in item.infoList.filter((e)=>!e.home)" class="action-detail" :data-id="visit.playerId" @click="toPlayerDetail" :key="index">
+                                                    <div class="icon"><i :class="actionType[visit.type].icon" :style="{'color':actionType[visit.type].color}" ></i></div>
+                                                    <div class="playerName">{{visit.name}}</div>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
+                                    <div class="dot"></div>
                                 </div>
+                                <i class="iconfont icon-koushao" style="color:#fc0;font-size:20px;"></i>
                             </div>
                         </div>
                         <div v-else style="height:100px;line-height:100px;text-align:center">暂无事件</div>
@@ -157,7 +178,12 @@ export default {
                     this.match = res.data;
                 }
             })
-        }
+        },
+        toPlayerDetail(e){
+            let id = e.currentTarget.dataset.id;
+            if(!id) return;
+            this.$router.push(`/player/${id}`);
+        },
     },
     computed: {
         dateDiff(){
@@ -180,14 +206,6 @@ export default {
 </script>
 
 <style scoped>
-.action_box{
-    display: flex;
-    font-size: 18px;
-    line-height:40px
-}
-.action_box div{
-    margin-left: 10px;
-}
 
 .description{
     display: flex;
@@ -210,6 +228,106 @@ export default {
     justify-content: center;
     align-items: center;
     color: #fff;
+}
+
+.action-wrap {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    border-bottom: 2px solid #f7f7f7;
+}
+
+.flex-center {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: auto;
+    margin: 0 20px;
+}
+
+.content {
+    width: 40px;
+}
+
+.line-wrapper {
+    margin : 8px 0;
+}
+
+.line {
+    height: 100%;
+    min-height: 40px;
+    width: 3px;
+    background-color: #fc0;
+}
+
+.dot {
+    min-width: 10px;
+    min-height: 10px;
+    border-radius: 50%;
+    background-color: #fc0;
+}
+
+.action-item {
+    display: flex;
+    justify-content: center;
+}
+
+.time-box {
+    font-size: 10px;
+    color: #fff;
+    background-color: #fc0;
+    border-radius: 50%;
+    min-height: 20px;
+    min-width: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.float-wrapper {
+    width: 0;
+}
+
+.float-right {
+    align-items : flex-start;
+}
+
+.float-left {
+    margin-left: -150px;
+    align-items : flex-end;
+}
+
+.action-list {
+    width: 150px;
+    padding: 20px;
+    border: 2px solid #f2f2f2;
+    border-radius: 4px;
+    margin-bottom: 16px;
+    padding: 10px 20px;
+    display: flex;
+    flex-direction: column;
+    box-sizing: border-box;
+}
+.action-detail{
+    display: flex;
+    width: auto;
+    margin: 2px 0;
+    cursor: pointer;
+}
+.align-right{
+    flex-direction: row-reverse;
+}
+.icon{
+    width: 28px;
+    height: 28px;
+    font-size: 28px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.playerName{
+    text-overflow: ellipsis;
+    white-space: nowrap;
 }
 
 </style>
